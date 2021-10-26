dd49fd752f3f02dc5f5175527d90aedd46347b6a
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-10-26 18:37:51 +0600

    Created a component "sidebar". Added code to css, html files of this component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
+    MovieComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
