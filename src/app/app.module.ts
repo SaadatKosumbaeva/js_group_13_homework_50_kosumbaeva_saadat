94f6af2b79dff45419fcd9d3f4f0922b64fc52f1
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-10-26 18:41:53 +0600

    Created a component "content". Added code to css, html files of this component. Inserted component into the app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
